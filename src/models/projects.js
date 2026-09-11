13c16f5d4095991a9f8e67ec970be105a11acfdb
byujupacuar <jcuizaarce@byupathway.edu>
2026-09-11 13:37:39 -0400

    weekp2

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,16 +2,18 @@ import db from './db.js'
 
 const getAllProjects = async() => {
     const query = `
-        SELECT 
+      SELECT 
       sp.id,
       sp.title,
       sp.description,
       sp.location,
       sp.project_date,
       sp.status,
-      o.name AS organization_name
+      o.name AS organization_name,
+      c.name AS category_name
     FROM service_projects sp
     JOIN organizations o ON sp.organization_id = o.id
+    LEFT JOIN categories c ON sp.category_id = c.id
     ORDER BY sp.project_date ASC;
     `;
 
