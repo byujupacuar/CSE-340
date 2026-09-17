70d6e0d27e1412205b9313e30ef2032bac859753
byujupacuar <jcuizaarce@byupathway.edu>
2026-09-17 16:03:50 -0400

    week03

---
+++ src/controllers/projects.js
@@ -0,0 +1,38 @@
+// Import any needed model functions
+import { getAllProjects, getProjectsByOrganizationId, getProjectDetails, getUpcomingProjects } from '../models/projects.js';
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5; // Define the number of upcoming projects to retrieve    
+// Define any controller functions
+const showProjectsPage = async (req, res) => {
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
+    const title = 'Upcoming Service Projects';
+
+    res.render('projects', { title, projects });
+};  
+
+const showProjectsByOrganizationPage = async (req, res) => {
+    const organizationId = req.params.id;
+    const projects = await getProjectsByOrganizationId(organizationId);
+
+    const title = 'Projects by Organization';
+
+    res.render('projects_by_organization', { title, projects });
+}
+
+const showProjectDetailsPage = async (req, res) => {
+    const projectId = req.params.id;
+    const projectDetails = await getProjectDetails(projectId);
+
+    if (!projectDetails) {
+        res.status(404).send('Project not found');
+        return;
+    }
+
+    const title = 'Project Details';
+
+    res.render('project', { title, projectDetails });
+};
+
+// Export any controller functions
+export { showProjectsPage, showProjectsByOrganizationPage, showProjectDetailsPage };
+
--- src/models/projects.js
+++ src/models/projects.js
@@ -3,18 +3,13 @@ import db from "./db.js";
 const getAllProjects = async () => {
   const query = `
       SELECT 
-      sp.id,
-      sp.title,
-      sp.description,
-      sp.location,
-      sp.project_date,
-      sp.status,
-      o.name AS organization_name,
-      c.name AS category_name
-    FROM service_projects sp
-    JOIN organizations o ON sp.organization_id = o.id
-    LEFT JOIN categories c ON sp.category_id = c.id
-    ORDER BY sp.project_date ASC;
+      id,
+      title,
+      description,
+      location,
+      project_date,
+      status
+    FROM service_projects;
     `;
 
   const result = await db.query(query);
@@ -22,4 +17,68 @@ const getAllProjects = async () => {
   return result.rows;
 };
 
-export { getAllProjects };
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date
+        FROM service_projects
+        WHERE organization_id = $1
+        ORDER BY project_date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
+
+const getUpcomingProjects = async (number_of_projects) => {
+  const query = `
+    SELECT
+      sp.id,
+      sp.title,
+      sp.description,
+      sp.project_date,
+      sp.location,
+      sp.organization_id,
+      o.name
+    FROM service_projects sp
+    JOIN organizations o ON sp.organization_id = o.id
+    WHERE sp.project_date >= CURRENT_DATE
+    ORDER BY sp.project_date ASC
+    LIMIT $1;
+  `;
+
+  const queryParams = [number_of_projects];
+  const result = await db.query(query, queryParams);
+
+  return result.rows;
+};
+
+const getProjectDetails = async (projectId) => {  
+  const query = `
+    SELECT
+      sp.id,
+      sp.title,
+      sp.description,
+      sp.project_date,
+      sp.location,
+      sp.organization_id,
+      o.name
+    FROM service_projects sp
+    JOIN organizations o ON sp.organization_id = o.id
+    WHERE sp.id = $1;
+  `;
+
+  const queryParams = [projectId];
+  const result = await db.query(query, queryParams);
+
+  return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
